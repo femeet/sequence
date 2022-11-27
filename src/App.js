13a8b1ac07f132c32bcf91ba233deedd9c7805bc
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-26 20:12:53 -0500

    Fetching from firestore works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import ControlContext from "./contexts/control-context";
 import Game from "./components/game/game";
 import Home from "./components/home/home";
 import appRoutes from "./shared/appRoutes";
+import db from "./index";
 
 class App extends Component {
   
@@ -17,9 +18,19 @@ class App extends Component {
         super();
         console.log('app starts');
     }
-    
+
+    componentDidMount() {
+
+        // Runs when the component mounts. Similar to useEffect Hook, but for a Component style class.
+
+        db.collection("games").get().then(querySnapshot => {
+            console.log(querySnapshot.docs.map(doc => doc.id)); // doc.id gives the ID
+            // doc.data() returns the data.
+        })
+
+    }
+
     render() {
-    
         const {
             board,
         } = this.state;
