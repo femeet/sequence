18d4a6855a0628e36b6b2ff8d5c819a879b49d07
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-26 20:42:52 -0500

    Card positions for hearts done

---
+++ src/shared/remaining_cards.js
@@ -0,0 +1,259 @@
+
+const remaining_cards = [
+    {
+        "suit": "hearts",
+        "value": 2,
+        "position": [
+            [4,4],
+            [7,1]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 3,
+        "position": [
+            [5,4],
+            [8,1]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 4,
+        "position": [
+            [5,5],
+            [8,2]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 5,
+        "position": [
+            [4,5],
+            [8,3]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 6,
+        "position": [
+            [3,5],
+            [8,4]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 7,
+        "position": [
+            [3,4],
+            [8,5]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 8,
+        "position": [
+            [3,3],
+            [8,6]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 9,
+        "position": [
+            [4,3],
+            [8,7]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": 10,
+        "position": [
+            [5,3],
+            [8,8]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": "J"
+    },
+    {
+        "suit": "hearts",
+        "value": "Q",
+        "position": [
+            [6,3],
+            [7,8]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": "K",
+        "position": [
+            [6,4],
+            [6,8]
+        ]
+    },
+    {
+        "suit": "hearts",
+        "value": "A",
+        "position": [
+            [6,5],
+            [5,8]
+        ]
+    },
+    {
+        "suit": "diamonds",
+        "value": 2
+    },
+    {
+        "suit": "diamonds",
+        "value": 3
+    },
+    {
+        "suit": "diamonds",
+        "value": 4
+    },
+    {
+        "suit": "diamonds",
+        "value": 5
+    },
+    {
+        "suit": "diamonds",
+        "value": 6
+    },
+    {
+        "suit": "diamonds",
+        "value": 7
+    },
+    {
+        "suit": "diamonds",
+        "value": 8
+    },
+    {
+        "suit": "diamonds",
+        "value": 9
+    },
+    {
+        "suit": "diamonds",
+        "value": 10
+    },
+    {
+        "suit": "diamonds",
+        "value": "J"
+    },
+    {
+        "suit": "diamonds",
+        "value": "Q"
+    },
+    {
+        "suit": "diamonds",
+        "value": "K"
+    },
+    {
+        "suit": "diamonds",
+        "value": "A"
+    },
+    {
+        "suit": "clubs",
+        "value": 2
+    },
+    {
+        "suit": "clubs",
+        "value": 3
+    },
+    {
+        "suit": "clubs",
+        "value": 4
+    },
+    {
+        "suit": "clubs",
+        "value": 5
+    },
+    {
+        "suit": "clubs",
+        "value": 6
+    },
+    {
+        "suit": "clubs",
+        "value": 7
+    },
+    {
+        "suit": "clubs",
+        "value": 8
+    },
+    {
+        "suit": "clubs",
+        "value": 9
+    },
+    {
+        "suit": "clubs",
+        "value": 10
+    },
+    {
+        "suit": "clubs",
+        "value": "J"
+    },
+    {
+        "suit": "clubs",
+        "value": "Q"
+    },
+    {
+        "suit": "clubs",
+        "value": "K"
+    },
+    {
+        "suit": "clubs",
+        "value": "A"
+    },
+    {
+        "suit": "spades",
+        "value": 2
+    },
+    {
+        "suit": "spades",
+        "value": 3
+    },
+    {
+        "suit": "spades",
+        "value": 4
+    },
+    {
+        "suit": "spades",
+        "value": 5
+    },
+    {
+        "suit": "spades",
+        "value": 6
+    },
+    {
+        "suit": "spades",
+        "value": 7
+    },
+    {
+        "suit": "spades",
+        "value": 8
+    },
+    {
+        "suit": "spades",
+        "value": 9
+    },
+    {
+        "suit": "spades",
+        "value": 10
+    },
+    {
+        "suit": "spades",
+        "value": "J"
+    },
+    {
+        "suit": "spades",
+        "value": "Q"
+    },
+    {
+        "suit": "spades",
+        "value": "K"
+    },
+    {
+        "suit": "spades",
+        "value": "A"
+    }
+]
