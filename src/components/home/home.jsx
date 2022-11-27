0141746e07c2a56be454d9bc31dbe13f0db8fa0e
Femin Dharamshi <femin.dharamshi@sakec.ac.in>
2022-11-27 00:22:25 -0500

    Firebase setup code added

---
+++ src/components/home/home.jsx
@@ -0,0 +1,63 @@
+import {useRef} from "react";
+import db from "../../index";
+import {doc, setDoc} from "firebase/firestore";
+import remaining_cards from "../../shared/remaining_cards";
+
+const Home = () => {
+
+    const textRef = useRef();
+
+    async function setupFirestore() {
+        const userName = textRef.current.value;
+        // TODO: Add validation for userName;
+
+        let newGameDoc = {};
+        let currentBoard = {};
+
+        for (let i = 0; i < 10; i++) {
+            var temp = [];
+            for (let j = 0; j < 10; j++) {
+                temp.push(0);
+            }
+            currentBoard[i] = temp;
+        }
+
+        newGameDoc['currentBoard'] = currentBoard;
+        newGameDoc['scoreMatrix'] = currentBoard;
+        newGameDoc['status'] = 0;
+
+        newGameDoc['player1'] = {
+            "name": userName
+        }
+
+        // newGameDoc['remaining_cards'] = [...remaining_cards, ...remaining_cards];
+
+        const gameID = Date.now().toString();
+
+        let remainingCards = [...remaining_cards, ...remaining_cards];
+
+        newGameDoc['gameID'] = gameID;
+        newGameDoc['remainingCards'] = remainingCards;
+
+        // Create a new game document in Firestore
+        await setDoc(doc(db, "games", gameID), newGameDoc);
+
+        // Save game ID, and player number in the localstorage
+        window.localStorage.setItem("gameID", gameID);
+        window.localStorage.setItem("playerID", "1");
+
+        // TODO: Go to game screen with Status 0 (To wait for other players)
+    }
+
+    return (
+        <div className={`home`}>
+            <input type="text" ref={textRef} placeholder="Enter your name"/>
+            <button onClick={() => {
+                setupFirestore();
+            }}>Create a Game
+            </button>
+        </div>
+    )
+}
+
+export default Home;
--- src/shared/remaining_cards.js
+++ src/shared/remaining_cards.js
@@ -3,74 +3,128 @@ const remaining_cards = [
     {
         "suit": "hearts",
         "value": 2,
-        "position": [
-            [4,4],
-            [7,1]
-        ]
+        "position": {
+            "0": [
+                4,
+                4
+            ],
+            "1": [
+                7,
+                1
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 3,
-        "position": [
-            [5,4],
-            [8,1]
-        ]
+        "position": {
+            "0": [
+                5,
+                4
+            ],
+            "1": [
+                8,
+                1
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 4,
-        "position": [
-            [5,5],
-            [8,2]
-        ]
+        "position": {
+            "0": [
+                5,
+                5
+            ],
+            "1": [
+                8,
+                2
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 5,
-        "position": [
-            [4,5],
-            [8,3]
-        ]
+        "position": {
+            "0": [
+                4,
+                5
+            ],
+            "1": [
+                8,
+                3
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 6,
-        "position": [
-            [3,5],
-            [8,4]
-        ]
+        "position": {
+            "0": [
+                3,
+                5
+            ],
+            "1": [
+                8,
+                4
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 7,
-        "position": [
-            [3,4],
-            [8,5]
-        ]
+        "position": {
+            "0": [
+                3,
+                4
+            ],
+            "1": [
+                8,
+                5
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 8,
-        "position": [
-            [3,3],
-            [8,6]
-        ]
+        "position": {
+            "0": [
+                3,
+                3
+            ],
+            "1": [
+                8,
+                6
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 9,
-        "position": [
-            [4,3],
-            [8,7]
-        ]
+        "position": {
+            "0": [
+                4,
+                3
+            ],
+            "1": [
+                8,
+                7
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": 10,
-        "position": [
-            [5,3],
-            [8,8]
-        ]
+        "position": {
+            "0": [
+                5,
+                3
+            ],
+            "1": [
+                8,
+                8
+            ]
+        }
     },
     {
         "suit": "hearts",
@@ -79,98 +133,170 @@ const remaining_cards = [
     {
         "suit": "hearts",
         "value": "Q",
-        "position": [
-            [6,3],
-            [7,8]
-        ]
+        "position": {
+            "0": [
+                6,
+                3
+            ],
+            "1": [
+                7,
+                8
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": "K",
-        "position": [
-            [6,4],
-            [6,8]
-        ]
+        "position": {
+            "0": [
+                6,
+                4
+            ],
+            "1": [
+                6,
+                8
+            ]
+        }
     },
     {
         "suit": "hearts",
         "value": "A",
-        "position": [
-            [6,5],
-            [5,8]
-        ]
+        "position": {
+            "0": [
+                6,
+                5
+            ],
+            "1": [
+                5,
+                8
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 2,
-        "position": [
-            [9,4],
-            [2,7]
-        ]
+        "position": {
+            "0": [
+                9,
+                4
+            ],
+            "1": [
+                2,
+                7
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 3,
-        "position": [
-            [3,7],
-            [9,3]
-        ]
+        "position": {
+            "0": [
+                3,
+                7
+            ],
+            "1": [
+                9,
+                3
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 4,
-        "position": [
-            [9,2],
-            [4,7]
-        ]
+        "position": {
+            "0": [
+                9,
+                2
+            ],
+            "1": [
+                4,
+                7
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 5,
-        "position": [
-            [9,1],
-            [5,7]
-        ]
+        "position": {
+            "0": [
+                9,
+                1
+            ],
+            "1": [
+                5,
+                7
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 6,
-        "position": [
-            [8,0],
-            [6,7]
-        ]
+        "position": {
+            "0": [
+                8,
+                0
+            ],
+            "1": [
+                6,
+                7
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 7,
-        "position": [
-            [7,7],
-            [7,0]
-        ]
+        "position": {
+            "0": [
+                7,
+                7
+            ],
+            "1": [
+                7,
+                0
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 8,
-        "position": [
-            [6,0],
-            [7,6]
-        ]
+        "position": {
+            "0": [
+                6,
+                0
+            ],
+            "1": [
+                7,
+                6
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 9,
-        "position": [
-            [5,0],
-            [7,5]
-        ]
+        "position": {
+            "0": [
+                5,
+                0
+            ],
+            "1": [
+                7,
+                5
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": 10,
-        "position": [
-            [4,0],
-            [7,4]
-        ]
+        "position": {
+            "0": [
+                4,
+                0
+            ],
+            "1": [
+                7,
+                4
+            ]
+        }
     },
     {
         "suit": "diamonds",
@@ -179,98 +305,170 @@ const remaining_cards = [
     {
         "suit": "diamonds",
         "value": "Q",
-        "position": [
-            [3,0],
-            [7,3]
-        ]
+        "position": {
+            "0": [
+                3,
+                0
+            ],
+            "1": [
+                7,
+                3
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": "K",
-        "position": [
-            [2,0],
-            [7,2]
-        ]
+        "position": {
+            "0": [
+                2,
+                0
+            ],
+            "1": [
+                7,
+                2
+            ]
+        }
     },
     {
         "suit": "diamonds",
         "value": "A",
-        "position": [
-            [1,0],
-            [6,2]
-        ]
+        "position": {
+            "0": [
+                1,
+                0
+            ],
+            "1": [
+                6,
+                2
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 2,
-        "position": [
-            [6,6],
-            [4,8]
-        ]
+        "position": {
+            "0": [
+                6,
+                6
+            ],
+            "1": [
+                4,
+                8
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 3,
-        "position": [
-            [5,6],
-            [3,8]
-        ]
+        "position": {
+            "0": [
+                5,
+                6
+            ],
+            "1": [
+                3,
+                8
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 4,
-        "position": [
-            [4,6],
-            [2,8]
-        ]
+        "position": {
+            "0": [
+                4,
+                6
+            ],
+            "1": [
+                2,
+                8
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 5,
-        "position": [
-            [3,6],
-            [1,8]
-        ]
+        "position": {
+            "0": [
+                3,
+                6
+            ],
+            "1": [
+                1,
+                8
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 6,
-        "position": [
-            [2,6],
-            [0,8]
-        ]
+        "position": {
+            "0": [
+                2,
+                6
+            ],
+            "1": [
+                0,
+                8
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 7,
-        "position": [
-            [2,5],
-            [0,7]
-        ]
+        "position": {
+            "0": [
+                2,
+                5
+            ],
+            "1": [
+                0,
+                7
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 8,
-        "position": [
-            [2,4],
-            [0,6]
-        ]
+        "position": {
+            "0": [
+                2,
+                4
+            ],
+            "1": [
+                0,
+                6
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 9,
-        "position": [
-            [2,3],
-            [0,5]
-        ]
+        "position": {
+            "0": [
+                2,
+                3
+            ],
+            "1": [
+                0,
+                5
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": 10,
-        "position": [
-            [2,2],
-            [0,4]
-        ]
+        "position": {
+            "0": [
+                2,
+                2
+            ],
+            "1": [
+                0,
+                4
+            ]
+        }
     },
     {
         "suit": "clubs",
@@ -279,99 +477,170 @@ const remaining_cards = [
     {
         "suit": "clubs",
         "value": "Q",
-        "position": [
-            [0,3],
-            [3,2]
-        ]
+        "position": {
+            "0": [
+                0,
+                3
+            ],
+            "1": [
+                3,
+                2
+            ]
+        }
     },
     {
         "suit": "clubs",
-        "value": "K"
-        ,
-        "position": [
-            [0,2],
-            [4,2]
-        ]
+        "value": "K",
+        "position": {
+            "0": [
+                0,
+                2
+            ],
+            "1": [
+                4,
+                2
+            ]
+        }
     },
     {
         "suit": "clubs",
         "value": "A",
-        "position": [
-            [0,1],
-            [5,2]
-        ]
+        "position": {
+            "0": [
+                0,
+                1
+            ],
+            "1": [
+                5,
+                2
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 2,
-        "position": [
-            [6,1],
-            [1,9]
-        ]
+        "position": {
+            "0": [
+                6,
+                1
+            ],
+            "1": [
+                1,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 3,
-        "position": [
-            [5,1],
-            [2,9]
-        ]
+        "position": {
+            "0": [
+                5,
+                1
+            ],
+            "1": [
+                2,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 4,
-        "position": [
-            [4,1],
-            [3,9]
-        ]
+        "position": {
+            "0": [
+                4,
+                1
+            ],
+            "1": [
+                3,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 5,
-        "position": [
-            [3,1],
-            [4,9]
-        ]
+        "position": {
+            "0": [
+                3,
+                1
+            ],
+            "1": [
+                4,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 6,
-        "position": [
-            [2,1],
-            [5,9]
-        ]
+        "position": {
+            "0": [
+                2,
+                1
+            ],
+            "1": [
+                5,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 7,
-        "position": [
-            [1,1],
-            [6,9]
-        ]
+        "position": {
+            "0": [
+                1,
+                1
+            ],
+            "1": [
+                6,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 8,
-        "position": [
-            [1,2],
-            [7,9]
-        ]
+        "position": {
+            "0": [
+                1,
+                2
+            ],
+            "1": [
+                7,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 9,
-        "position": [
-            [1,3],
-            [8,9]
-        ]
+        "position": {
+            "0": [
+                1,
+                3
+            ],
+            "1": [
+                8,
+                9
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": 10,
-        "position": [
-            [9,8],
-            [1,4]
-        ]
+        "position": {
+            "0": [
+                9,
+                8
+            ],
+            "1": [
+                1,
+                4
+            ]
+        }
     },
     {
         "suit": "spades",
@@ -380,25 +649,45 @@ const remaining_cards = [
     {
         "suit": "spades",
         "value": "Q",
-        "position": [
-            [9,7],
-            [1,5]
-        ]
+        "position": {
+            "0": [
+                9,
+                7
+            ],
+            "1": [
+                1,
+                5
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": "K",
-        "position": [
-            [9,6],
-            [1,6]
-        ]
+        "position": {
+            "0": [
+                9,
+                6
+            ],
+            "1": [
+                1,
+                6
+            ]
+        }
     },
     {
         "suit": "spades",
         "value": "A",
-        "position": [
-            [9,5],
-            [1,7]
-        ]
+        "position": {
+            "0": [
+                9,
+                5
+            ],
+            "1": [
+                1,
+                7
+            ]
+        }
     }
 ]
+
+export default remaining_cards;
